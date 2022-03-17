a093aecc080945c52339a334140ec00c15bd40e2
shandarov <101797372+shandarov@users.noreply.github.com>
2022-03-17 17:02:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("js/sw.js")
+    navigator.serviceWorker.register("sw.js")
         .then(() => console.log("Зарегистрировали"))
         .catch(() => console.log("Получилась ошибка"));
 }
